4cf34f235bade91773f503dc36cb2efbc1541156
Sanberk Türker <69405619+crealyx@users.noreply.github.com>
2021-10-09 01:25:56 +0300

    Add skew animation

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,23 @@
 import Arrows from './Arrows';
 import Name from './Name';
-
+import { motion } from 'framer-motion';
 const Home = () => {
   return (
     <section id="home" className="home">
       <Name />
       <Arrows href="#about" />
-      <div className="skew-1"></div>
-      <div className="skew-2"></div>
+      <motion.div
+        initial={{ x: -3500 }}
+        animate={{ x: 50, y: -15, rotate: '-3deg', skewX: '-70deg' }}
+        transition={{ delay: 0.2, duration: 1 }}
+        className="skew-1"
+      ></motion.div>
+      <motion.div
+        initial={{ x: -2500 }}
+        animate={{ x: 300 }}
+        transition={{ delay: 1, duration: 1 }}
+        className="skew-2"
+      ></motion.div>
     </section>
   );
 };
