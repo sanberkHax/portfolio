674c5335c7a857ab0ea05c28d38872c8a6f0c257
Sanberk Türker <69405619+sanberkHax@users.noreply.github.com>
2022-03-05 18:34:39 +0300

    Cleanup Animate on Scroll code

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,5 @@
 import '../stylesheets/main.scss';
-import AOS from 'aos';
-import { useEffect } from 'react';
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    AOS.init({
-      offset: 400,
-      duration: 1000,
-    });
-  }, []);
   return <Component {...pageProps} />;
 }
 
